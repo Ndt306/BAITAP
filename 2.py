a6d370561499add9f22053894332b3190ae2bad4
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-08 20:36:16 +0700

    Bai4

--- 2.py
+++ 2.py
@@ -2,15 +2,19 @@ print("Sinh viên: Ngo Duc Thang")
 print("Ma so Sv: 245751030310019")
 print("########################")
 
-import turtle, random
-colors=["red","green","blue","orange","purple","pink","yellow"]
-painter = turtle.Turtle()
-painter.pensize(3)
-for i in range(10):
-    color = random.choice(colors)
-    painter.pencolor(color)
-    painter.circle(100)
-    painter.right(30)
-    painter.left(60)
-    painter.setposition(0, 0)
+class Hinhchunhat:
+    def __init__(self, chieu_dai, chieu_rong):
+        self.chieu_dai = chieu_dai
+        self.chieu_rong = chieu_rong
+
+    def dien_tich(self):
+        return self.chieu_dai * self.chieu_rong
+
+hcn = Hinhchunhat(10, 6)
+print("Diện tích hình chữ nhật là:", hcn.dien_tich())
+    
+ 
+ 
+ 
+ 
 
