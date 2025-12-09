9dfa9ddb80915f4763637e9da901da51691fd392
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-09 15:18:05 +0700

    Bai4

--- 3.py
+++ 3.py
@@ -2,15 +2,23 @@ print("Sinh viên: Ngo Duc Thang")
 print("Ma so Sv: 245751030310019")
 print("########################")
 
-import turtle, random
-colors=["red","green","blue"]
-painter = turtle.Turtle()
-painter.pensize(3)
-for i in range(12):
-    color = colors[i % len(colors)]
-    painter.pencolor(color)
-    painter.circle(100)
-    painter.right(30)
-    painter.left(60)
-    painter.setposition(0, 0)
+class Nguoi:
+    def getGender(self):
+        return "Unknown"
+
+class Nam(Nguoi):
+    def getGender(self):
+        return "Nam"
+
+class Nu(Nguoi):
+    def getGender(self):
+        return "Nữ"
+a = Nam()
+b = Nu()
+print(a.getGender())
+print(b.getGender())
+ 
+ 
+ 
+ 
 
