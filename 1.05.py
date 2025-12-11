ddc8dcc7b72ac86277c522e11acdc47b1b460dc5
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:50:44 +0700

    Bai1

---
+++ 1.05.py
@@ -0,0 +1,8 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+n=int(input("Enter A Number--->"))
+while n>=0:
+    print(n)
+    n = n - 1
