c61b04c79fbdc04eda6cb5d495070df0ec734655
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:33:45 +0700

    Bai2

---
+++ 2.03.py
@@ -0,0 +1,9 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+def say_hello():
+    a = "Hello"
+    print(a)
+    print(a)
+    print(a)
