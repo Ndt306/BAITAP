c6333677fbab855d823dd22439a036b7433cb6e0
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 15:32:36 +0700

    Bai3

---
+++ 3.07.py
@@ -0,0 +1,8 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+S = input("Nhap choi:")
+for ch in S:
+    if not ch.isdigit():
+        print(ch)
