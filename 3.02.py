a806677db8825a978a0d8935014ab259587393e7
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 15:42:57 +0700

    Bai3

---
+++ 3.02.py
@@ -0,0 +1,8 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+S = input("Nhap chuoi:")
+for ch in S:
+    if ch not in ["","\t"]:
+        print(ch)
