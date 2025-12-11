4564ff84f6224c7034c87ace3e3c53cc8cc0a478
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 15:11:10 +0700

    Bai5

---
+++ 3.15.py
@@ -0,0 +1,9 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+s = input()
+words = s.split()
+words.sort()
+for w in words:
+    print(w)
