15516ba8adddc682fb080444e3dd7b179f49d67b
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 14:40:33 +0700

    Bai3

---
+++ 3.19.py
@@ -0,0 +1,16 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+n = 1_000_000
+prime = [True] * n
+prime[0] = prime[1] = False
+
+for i in range(2, int(n**0.5) + 1):
+    if prime[i]:
+        for j in range(i*i, n, 1):
+            prime[j] = False
+
+P = tuple(i for i in range(n) if prime[i])
+print("Tuple P đã tạo xong!")
+print("Số lượng số nguyên tố:", len(P))
