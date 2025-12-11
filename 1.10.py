a3f9987e1b1ca3cc8004a38bc0866a432a962a10
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 22:15:41 +0700

    bai1

---
+++ 1.10.py
@@ -0,0 +1,9 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+a=input("Nhập xâu kí tự:")
+b=a.split()
+print(b)
+c= " ".join(b)
+print(c)
