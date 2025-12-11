6d61ff60f02d7f022a1dca6c1dc15bc2578b1d29
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 22:18:33 +0700

    bai1

---
+++ 1.04.py
@@ -0,0 +1,9 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+i=1
+for j in range(2,10):
+    print("i:",i,"j:",j)
+    print(i,"/",j)
+    print(i/j)
