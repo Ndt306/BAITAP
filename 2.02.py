9b487c09892b8fbf2e29f9a329585456f3086cd9
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:34:16 +0700

    Bai2

---
+++ 2.02.py
@@ -0,0 +1,8 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+def sum(a, b):
+    return a+b
+c = sum(8,1);
+print("Tổng cua 8 va 1 =" + str(c))
