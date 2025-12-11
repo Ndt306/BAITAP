203b9d49d84e788fe963736df2caa3f2e4cbd0c9
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 22:16:22 +0700

    bai1

---
+++ 1.11.py
@@ -0,0 +1,12 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+x=[1,"python",4,7]
+y=["cse",2,"guntur",8]
+m=[]
+m.append(x)
+m.append(y)
+print(m)
+d={1:1,2:x,"combine_list":m}
+print(d)
