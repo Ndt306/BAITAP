5487956aed9955263ed4cb16d4070eea987b05b8
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:32:44 +0700

    Bai2

---
+++ 2.07.py
@@ -0,0 +1,10 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+def checkValue(n):
+    if n % 2 ==0:
+        print("Đây là số chẵn")
+    else:
+        print("Đây là số lẻ")
+checkValue(10)
