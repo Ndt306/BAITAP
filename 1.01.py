b613bf69d3dfe67381452235f7e1075b02b1d252
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:49:43 +0700

    Bai1

---
+++ 1.01.py
@@ -0,0 +1,11 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+n1 =int(input("enter n1 value"))
+n2 =int(input("Enter n2 value"))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
+    
