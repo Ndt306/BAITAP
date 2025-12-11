e19a1f80ae346274c4a721c00de5dab5bdf9c2a2
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 22:18:20 +0700

    Delete 1.04.py

--- 1.04.py
+++
@@ -1,9 +0,0 @@
-print("Sinh viên: Ngo Duc Thang")
-print("Ma so Sv: 245751030310019")
-print("########################")
-
-i=1;
-for j in range(2,10):
-    print("i:",i,"j:",j)
-    print(i,"/",j)
-    print(i/j)
