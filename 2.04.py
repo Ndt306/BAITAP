ddad2f9dd7f1459b87310a3a86a001d77b880b72
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:33:32 +0700

    Bai2

---
+++ 2.04.py
@@ -0,0 +1,10 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+a = "Hello Guy!"
+def say(a):
+    a = "Vinh University"
+    print(a)
+say(a)
+print(a)
