c3e3a08f036c0097811cf18b7e8b754c4e2b8ee9
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 21:50:16 +0700

    Bai1

---
+++ 1.03.py
@@ -0,0 +1,9 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+n=int(input("Enter a number--->"))
+if n % 2==0:
+    print("EVEN number");
+else:
+    print("ODD number");
