ae4ed7f2c06be6d0da765e893e5e55b308d08022
N_Thắngk6 <ndt1682006@gmail.com>
2025-12-11 22:06:16 +0700

    bai1

---
+++ 1.07.py
@@ -0,0 +1,7 @@
+print("Sinh viên: Ngo Duc Thang")
+print("Ma so Sv: 245751030310019")
+print("########################")
+
+n =int(input("Nhập số nguyên n:"))
+d ={i:i*i for i in range(1, n + 1)}
+print(d)
